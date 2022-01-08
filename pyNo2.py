405a30cab4bb5a0801cc7958edb4a88ef1400581
can <ttmcan@hotmail.com>
2022-01-08 15:39:28 +0300

    pyNo2 eklendi

---
+++ pyNo2.py
@@ -0,0 +1,10 @@
+docput=input('please enter doc path or doc name if it is in the same folder \n')
+fhand=open(,encoding='utf-8')
+inp=fhand.read()
+
+print("system will show the text till that word")
+nput=input('please enter the word \n')
+
+n=inp.find(nput)
+d=int(n)
+print(inp[:d])
